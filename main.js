6d2c399e98124b5a8c060e1c339f1f6f9d60b698
Sam Daigle <110954530+samdaigle@users.noreply.github.com>
2026-03-26 15:18:22 -0400

    add preliminary javascript variables

---
+++ main.js
@@ -0,0 +1,12 @@
+const income = document.querySelector(
+  ".summary__item--balance > .summary__value",
+);
+const expenses = document.querySelector(
+  ".summary__item--expense > .summary__value",
+);
+
+const balance = document.querySelector(
+  ".summary__item--balance > .summary__value",
+);
+
+const addTransactionBtn = document.querySelector(".btn--primary");
